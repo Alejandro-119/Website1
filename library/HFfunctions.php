86f3edf51f4ea5bf8f1c1235b0a3211c3cd180b5
Alejandro Herrera -Aug-17-2018 <aherrera13@bcst01.parkland.edu>
2021-03-09 18:27:58 -0600

    library

---
+++ library/HFfunctions.php
@@ -0,0 +1,32 @@
+<!--I honor Parkland's core values by affirming that I have followed all academic integrity guidelines for this work.
+       Alejandro Herrera, CSC155201F -->
+<?php
+function head()
+{
+   echo '<br>CSC155201F';
+   echo '<br>Alejandro Herrera';
+   echo '<br><img src="library/Goldeneagle.jpg" height="190" width="280">';
+}
+?>
+
+<?php
+function footer()
+{
+   echo '<br><a href="http://www.csit.parkland.edu/~aherrera13/csc155/12labs/dogpage.php">Dog Page</a>';
+   echo ' | ';
+   echo '<a href="http://www.csit.parkland.edu/~aherrera13/csc155/12labs/catpage.php">Cat Page</a>';
+   echo ' | ';
+   echo '<a href="http://www.csit.parkland.edu/~aherrera13/csc155/12labs/birdpage.php">Bird Page</a>';
+   echo ' | ';
+   echo '<a href="http://www.csit.parkland.edu/~aherrera13/csc155/12labs/snakepage.php">Snake Page</a>';
+   echo ' | ';
+   echo '<a href="http://www.csit.parkland.edu/~aherrera13/csc155/12labs/loginpage.php">Login Page</a>';
+   echo ' | ';
+   echo '<a href="http://www.csit.parkland.edu/~aherrera13/csc155/12labs/welcomepage.php">Welcome Page</a>';
+   echo ' | ';
+   echo '<a href="http://www.csit.parkland.edu/~aherrera13/csc155/12labs/logoutpage.php">Logout Page</a>';
+   echo ' | ';
+   echo '<a href="http://www.csit.parkland.edu/~aherrera13/csc155/12labs/shoppingcartpage.php">Shopping Cart</a>';
+
+}
+?>
